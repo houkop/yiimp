b523d0dcd55fe3f0baa032b57c51610b1dfc0702
houkop <hompfull@gmail.com>
2017-12-04 16:31:47 +0300

    Update coin_form.php

--- web/yaamp/modules/site/coin_form.php
+++ web/yaamp/modules/site/coin_form.php
@@ -381,7 +381,7 @@ if ($coin->id) {
 	echo "gen=0\n";
 	echo "\n";
 	echo "alertnotify=echo %s | mail -s \"{$coin->name} alert!\" ".YAAMP_ADMIN_EMAIL."\n";
-	echo "blocknotify=blocknotify ".YAAMP_STRATUM_URL.":$port {$coin->id} %s\n";
+	echo "blocknotify=/var/stratum/blocknotify ".YAAMP_STRATUM_URL.":$port {$coin->id} %s\n";
 	echo CHtml::closetag("pre");
 
 	echo CHtml::tag("hr");
